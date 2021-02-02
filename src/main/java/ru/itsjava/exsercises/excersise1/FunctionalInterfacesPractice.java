13f0c8e4971f63c6c27e03c2bf67d120c4ae560d
Vitaliy <SeekerOfConfessor@gmail.com>
2021-02-02 14:13:04 +0300

    delete solutions into ex

--- src/main/java/ru/itsjava/exsercises/excersise1/FunctionalInterfacesPractice.java
+++ src/main/java/ru/itsjava/exsercises/excersise1/FunctionalInterfacesPractice.java
@@ -28,20 +28,7 @@ public class FunctionalInterfacesPractice {
         //который проверяет является ли первый символ у строки числом
         //Воспользоваться Character.isDigit()
 
-        Predicate<String> isFirstCharDigit = (str) -> {
-            char firstSymbol = str.charAt(0);
-            return Character.isDigit(firstSymbol);
-        };
-
-        Predicate<String> isFirstCharDigit2 = str ->
-                Character.isDigit(str.charAt(0));
-
-
-        BinaryOperator<Long> binarySumOperator = (x, y) -> x + y;
-        System.out.println("binarySumOperator.apply(5L,4L) = " + binarySumOperator.apply(5L, 4L));
 
         //Написать свой оператор разности для типа int
-        BinaryOperator<Integer> binaryResOperator = (x, y) -> x - y;
-        System.out.println("binaryResOperator.apply(5,4) = " + binaryResOperator.apply(5, 4));
     }
 }
